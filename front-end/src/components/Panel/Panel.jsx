1201bef29656f74f9258365d09b2262f9540ec3a
Amadeusz Majcher <amadeuszmajcher@gmail.com>
2024-11-28 20:14:19 +0100

    feat: adding filtering capabilities

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -2,23 +2,28 @@ import { useState, useEffect } from "react";
 import { List } from "../List/List";
 import { Form } from "../Form/Form";
 import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
+import { FilterButton } from "../FilterButton/FilterButton";
 import styles from "./Panel.module.css";
 
 export function Panel() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const URL = "http://localhost:3000/words";
+
   useEffect(() => {
-    fetch("http://localhost:3000/words")
+    const params = selectedCategory ? `?category=${selectedCategory}` : "";
+    fetch(`${URL}${params}`)
       .then((res) => res.json())
       .then((res) => {
         setData(res);
         setIsLoading(false);
       });
-  }, []);
+  }, [selectedCategory]);
 
   function handleFormSubmit(formData) {
-    fetch("http://localhost:3000/words", {
+    fetch(URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -27,12 +32,14 @@ export function Panel() {
     })
       .then((res) => res.json())
       .then((res) => {
-        setData((prevData) => [...prevData, res]);
+        if (!selectedCategory || selectedCategory === res.category) {
+          setData((prevData) => [...prevData, res]);
+        }
       });
   }
 
   function handleDeleteItem(id) {
-    fetch(`http://localhost:3000/words/${id}`, {
+    fetch(`${URL}/${id}`, {
       method: "DELETE",
     })
       .then((res) => {
@@ -50,6 +57,10 @@ export function Panel() {
       });
   }
 
+  function handleFilterClick(category) {
+    setSelectedCategory(category);
+  }
+
   if (isLoading) {
     return <p>Ładowanie</p>;
   }
@@ -58,6 +69,26 @@ export function Panel() {
       {error && <ErrorMessage>{error}</ErrorMessage>}
       <section className={styles.section}>
         <Form onFormSubmit={handleFormSubmit} />
+        <div className={styles.filter}>
+          <FilterButton
+            active={selectedCategory === null}
+            onClick={() => handleFilterClick(null)}
+          >
+            Wszystkie
+          </FilterButton>
+          <FilterButton
+            active={selectedCategory === "noun"}
+            onClick={() => handleFilterClick("noun")}
+          >
+            Rzeczowniki
+          </FilterButton>
+          <FilterButton
+            active={selectedCategory === "verb"}
+            onClick={() => handleFilterClick("verb")}
+          >
+            Czasowniki
+          </FilterButton>
+        </div>
         <List data={data} onDeleteItem={handleDeleteItem} />
       </section>
     </>
